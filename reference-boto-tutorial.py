17f7422fb2e9fdcb151c39b97bf8b8a3356b3fe6
s4ayub <shababayub@gmail.com>
2016-12-11 01:24:24 -0500

    added scripts from references on boto tutorial

---
+++ reference-boto-tutorial.py
@@ -0,0 +1,185 @@
+# SOURCE ---> http://www.toforge.com/tag/mturk/
+
+from boto.mturk.connection import MTurkConnection
+from boto.mturk.question import QuestionContent,Question,QuestionForm,
+Overview,AnswerSpecification,SelectionAnswer,FormattedContent,FreeTextAnswer
+
+ACCESS_ID ='your acces key'
+SECRET_KEY = 'your secret key'
+HOST = 'mechanicalturk.sandbox.amazonaws.com'
+
+mtc = MTurkConnection(aws_access_key_id=ACCESS_ID,
+                      aws_secret_access_key=SECRET_KEY,
+                      host=HOST)
+
+title = 'Give your opinion about a website'
+description = ('Visit a website and give us your opinion about'
+               ' the design and also some personal comments')
+keywords = 'website, rating, opinions'
+
+ratings =[('Very Bad','-2'),
+         ('Bad','-1'),
+         ('Not bad','0'),
+         ('Good','1'),
+         ('Very Good','1')]
+
+#---------------  BUILD OVERVIEW -------------------
+
+overview = Overview()
+overview.append_field('Title', 'Give your opinion on this website')
+overview.append(FormattedContent('<a target="_blank"'
+                                 ' href="http://www.toforge.com">'
+                                 ' Mauro Rocco Personal Forge</a>'))
+
+#---------------  BUILD QUESTION 1 -------------------
+
+qc1 = QuestionContent()
+qc1.append_field('Title','How looks the design ?')
+
+fta1 = SelectionAnswer(min=1, max=1,style='dropdown',
+                      selections=ratings,
+                      type='text',
+                      other=False)
+
+q1 = Question(identifier='design',
+              content=qc1,
+              answer_spec=AnswerSpecification(fta1),
+              is_required=True)
+
+#---------------  BUILD QUESTION 2 -------------------
+
+qc2 = QuestionContent()
+qc2.append_field('Title','Your personal comments')
+
+fta2 = FreeTextAnswer()
+
+q2 = Question(identifier="comments",
+              content=qc2,
+              answer_spec=AnswerSpecification(fta2))
+
+#--------------- BUILD THE QUESTION FORM -------------------
+
+question_form = QuestionForm()
+question_form.append(overview)
+question_form.append(q1)
+question_form.append(q2)
+
+#--------------- CREATE THE HIT -------------------
+
+mtc.create_hit(questions=question_form,
+               max_assignments=1,
+               title=title,
+               description=description,
+               keywords=keywords,
+               duration = 60*5,
+               reward=0.05)
+
+
+
+
+def get_all_reviewable_hits(mtc):
+    page_size = 50
+    hits = mtc.get_reviewable_hits(page_size=page_size)
+    print "Total results to fetch %s " % hits.TotalNumResults
+    print "Request hits page %i" % 1
+    total_pages = float(hits.TotalNumResults)/page_size
+    int_total= int(total_pages)
+    if(total_pages-int_total>0):
+        total_pages = int_total+1
+    else:
+        total_pages = int_total
+    pn = 1
+    while pn < total_pages:
+        pn = pn + 1
+        print "Request hits page %i" % pn
+        temp_hits = mtc.get_reviewable_hits(page_size=page_size,page_number=pn)
+        hits.extend(temp_hits)
+    return hits
+&#91;/python&#93;
+
+The list of hits returned by the method is a list of boto HITS objects.
+This object doesn't contain the assignments, you have to call another method for get the assignments of a particular HIT id.
+The next step is tho iterate trough this list and for each HIT calls the method <a href="http://boto.cloudhackers.com/ref/mturk.html#boto.mturk.connection.MTurkConnection.get_assignments" target="_blank">get_assignments(hit_id)</a>
+
+This method will return all the answers to your hits.
+Below the complete script for print to screen all the assignments of your hits.
+
+
+from boto.mturk.connection import MTurkConnection
+
+ACCESS_ID ='your access id'
+SECRET_KEY = 'your key'
+HOST = 'mechanicalturk.sandbox.amazonaws.com'
+
+def get_all_reviewable_hits(mtc):
+    page_size = 50
+    hits = mtc.get_reviewable_hits(page_size=page_size)
+    print "Total results to fetch %s " % hits.TotalNumResults
+    print "Request hits page %i" % 1
+    total_pages = float(hits.TotalNumResults)/page_size
+    int_total= int(total_pages)
+    if(total_pages-int_total>0):
+        total_pages = int_total+1
+    else:
+        total_pages = int_total
+    pn = 1
+    while pn < total_pages:
+        pn = pn + 1
+        print "Request hits page %i" % pn
+        temp_hits = mtc.get_reviewable_hits(page_size=page_size,page_number=pn)
+        hits.extend(temp_hits)
+    return hits
+
+mtc = MTurkConnection(aws_access_key_id=ACCESS_ID,
+                      aws_secret_access_key=SECRET_KEY,
+                      host=HOST)
+
+hits = get_all_reviewable_hits(mtc)
+
+for hit in hits:
+    assignments = mtc.get_assignments(hit.HITId)
+    for assignment in assignments:
+        print "Answers of the worker %s" % assignment.WorkerId
+        for question_form_answer in assignment.answers&#91;0&#93;:
+            for key, value in question_form_answer.fields:
+                print "%s: %s" % (key,value)
+        print "--------------------"
+&#91;/python&#93;
+
+# As you can see the scripts call the get_assignments method for each hit id and after that iterate trough it for fetching the answers.
+# In the line 36 you see an answer&#91;0&#93;, maybe you are thinking "why don't iterate trough all answers ?"
+# For try to give a clear explanation first let's give some definition thaw will be valid on the next rows.
+# <ul>
+#     <li>A "question form answer" is the single answer to a single question of your form.</li>
+#     <li>An "answer" element is the set of all the "question form answer" of your QuestionForm</li>
+#     <li>An "assignment" is the set of all the "answer<strong>s</strong>" of the same worker</li>
+# </ul>
+# In practice each worker can give just 1 "answer" to the hit, for that the assignment will contain always just one "answer".
+# "answer<strong>s</strong>" element is just a reflection of the xml structure, boto translate it as array of one element.
+# If this explanation has been clear, you just have to know which method use for accept and refuse payments to the workers.
+# The operations of pay and refuse have do be done on the "assignments" unit, in fact they accept the assignment id as a parameter.
+
+# <tt>approve_assignment</tt><big>(</big><em>assignment_id</em>, <em>feedback=None</em><big>)</big>
+
+# <tt>reject_assignment</tt><big>(</big><em>assignment_id</em>, <em>feedback=None</em><big>)</big>
+
+# Both methods accept also a feedback string, this is the message that the workers will receive as explanation for the approved/rejected assignment, be kind <img draggable="false" class="emoji" alt="😀" src="https://s.w.org/images/core/emoji/72x72/1f600.png"> .
+# When you don't need anymore an hits you can "delete" it from mturk by calling the method
+
+# <tt>disable_hit</tt><big>(</big><em>hit_id</em>, <em>response_groups=None</em><big>)</big>
+
+# I suggest you to read the <a href="http://boto.cloudhackers.com/ref/mturk.html#boto.mturk.connection.MTurkConnection.disable_hit" target="_blank">documentation</a> about disable_hit method.
+# I leave you with an edited version of the loop that pay all workers and disable the hits.
+# See you soon <img draggable="false" class="emoji" alt="😉" src="https://s.w.org/images/core/emoji/72x72/1f609.png">
+
+
+for hit in hits:
+    assignments = mtc.get_assignments(hit.HITId)
+    for assignment in assignments:
+        print "Answers of the worker %s" % assignment.WorkerId
+        for question_form_answer in assignment.answers[0]:
+            for key, value in question_form_answer.fields:
+                print "%s: %s" % (key,value)
+        mtc.approve_assignment(assignment.AssignmentId)
+        print "--------------------"
+    mtc.disable_hit(hit.HITId)
---
+++ reference-crowdsource-tasks.py
@@ -0,0 +1,118 @@
+# SOURCE ---------> http://scottlobdell.me/2015/03/use-mechanical-turk-python-boto-crowdsource-tasks/
+
+
+import os
+from boto.mturk.connection import MTurkConnection
+from boto.mturk.question import ExternalQuestion
+from boto.mturk.price import Price
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+
+if os.environ.get("I_AM_IN_DEV_ENV"):
+    HOST = 'mechanicalturk.sandbox.amazonaws.com'
+else:
+    HOST = 'mechanicalturk.amazonaws.com'
+
+connection = MTurkConnection(aws_access_key_id=AWS_ACCESS_KEY_ID,
+                             aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                             host=HOST)
+
+url = "https://mturk-demonstration.herokuapp.com/"
+title = "Describe a picture in your own words (COMPLETE THIS TASK ONLY ONCE!)"
+description = "COMPLETE THIS TASK ONLY ONCE! All submissions after the first will be rejected"
+keywords = ["easy"]
+frame_height = 800
+amount = 0.05
+
+questionform = ExternalQuestion(url, frame_height)
+
+
+for _ in xrange(60):
+    create_hit_result = connection.create_hit(
+        title=title,
+        description=description,
+        keywords=keywords,
+        max_assignments=1,
+        question=questionform,
+        reward=Price(amount=amount),
+        response_groups=('Minimal', 'HITDetail'),  # I don't know what response groups are
+    )
+
+# SERVER SIDE CODE
+
+if os.environ.get("I_AM_IN_DEV_ENV"):
+    AMAZON_HOST = "https://workersandbox.mturk.com/mturk/externalSubmit"
+else:
+    AMAZON_HOST = "https://www.mturk.com/mturk/externalSubmit"
+
+
+def home(request):
+    if request.GET.get("assignmentId") == "ASSIGNMENT_ID_NOT_AVAILABLE":
+        # worker hasn't accepted the HIT (task) yet
+        pass
+    else:
+        # worked accepted the task
+        pass
+
+    worker_id = request.GET.get("workerId", "")
+    if worker_id in get_worker_ids_past_tasks():
+        # you might want to guard against this case somehow
+        pass
+
+    render_data = {
+        "worker_id": request.GET.get("workerId", ""),
+        "assignment_id": request.GET.get("assignmentId", ""),
+        "amazon_host": AMAZON_HOST,
+        "hit_id": request.GET.get("hitId", ""),
+    }
+
+    response = render_to_response("base.html", render_data)
+    # without this header, your iFrame will not render in Amazon
+    response['x-frame-options'] = 'this_can_be_anything'
+    return response
+
+# CLIENT SIDE CODE
+
+# <html>
+#     <head>
+#         {% include "_js_scripts.html" %}
+#     </head>
+#     <body>
+#     <h3>Using your own words, describe this man however you want.</h3>
+#         <form action="{{ amazon_host }}" method="POST">
+#             <textarea rows="4" cols="50" name="user-input">
+#             </textarea>
+#             <input type="hidden" id="assignmentId" value="{{ assignment_id }}" name="assignmentId"/>
+#             <br/>
+#             <input type="hidden" id="workerId" value="{{ worker_id }}" name="workerId"/>
+#             <input type="hidden" id="hitId" value="{{ hit_id }}" name="hitId"/>
+#             <input type="submit">
+#         </form>
+#         <div>
+#             <img src="https://s3.amazonaws.com/lobbdawg/mturk/broome1.jpg" style="width: 30%;">
+#             <img src="https://s3.amazonaws.com/lobbdawg/mturk/broome2.jpg" style="width: 30%;">
+#             <img src="https://s3.amazonaws.com/lobbdawg/mturk/broome3.jpg" style="width: 30%;">
+#             <img src="https://s3.amazonaws.com/lobbdawg/mturk/broome4.jpg" style="width: 30%;">
+#             <img src="https://s3.amazonaws.com/lobbdawg/mturk/broome5.jpg" style="width: 30%;">
+#             <img src="https://s3.amazonaws.com/lobbdawg/mturk/broome6.jpg" style="width: 30%;">
+#         </div>
+#     </body>
+# </html>
+
+# MANIPULATING HITs
+
+all_hits = [hit for hit in connection.get_all_hits()]
+
+for hit in all_hits:
+    assignments = connection.get_assignments(hit.HITId)
+    for assignment in assignments:
+        # don't ask me why this is a 2D list
+        question_form_answers = assignment.answers[0]
+        for question_form_answer in question_form_answers:
+            # "user-input" is the field I created and the only one I care about
+            if question_form_answer.qid == "user-input":
+                user_response = question_form_answer.fields[0]
+                print user_response
+                print "\n"
+        # connection.approve_assignment(assignment.AssignmentId)
